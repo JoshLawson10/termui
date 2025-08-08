79db40e68d19ba542e857886e06c48f7e28327a4
Josh Lawson <joshlawson100@outlook.com>
2025-08-09 08:45:47 +1000

    Added lighten and darken methods

--- src/termui/colors/rgb.py
+++ src/termui/colors/rgb.py
@@ -12,7 +12,27 @@ class RGBColor:
     blue: int
 
     def __repr__(self):
-        return f"RGBC({self.red}, {self.green}, {self.blue})"
+        return f"RGB({self.red}, {self.green}, {self.blue})"
 
     def to_tuple(self):
         return (self.red, self.green, self.blue)
+
+    def lighten(self, percent: float) -> "RGBColor":
+        """
+        Lightens the color by a given percentage.
+        """
+        return RGBColor(
+            red=min(255, int(self.red + self.red * percent)),
+            green=min(255, int(self.green + self.green * percent)),
+            blue=min(255, int(self.blue + self.blue * percent)),
+        )
+
+    def darken(self, percent: float) -> "RGBColor":
+        """
+        Darkens the color by a given percentage.
+        """
+        return RGBColor(
+            red=max(0, int(self.red - self.red * percent)),
+            green=max(0, int(self.green - self.green * percent)),
+            blue=max(0, int(self.blue - self.blue * percent)),
+        )
