906db12451971fc0a567c9ded801ade2de4f313b
Josh Lawson <joshlawson100@outlook.com>
2025-08-11 18:02:21 +1000

    Removed unused 'theme' class

--- src/termui/theme.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-from termui.color import Color
-
-
-class ColorTheme:
-    def __init__(self, background_color: Optional[AnsiColor | RGBColor] = None) -> None:
-        self.background_color = background_color
